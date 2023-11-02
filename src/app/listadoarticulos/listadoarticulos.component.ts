261d15486aded677e91946d60ad552c05941d008
angelSantori <antonyo.da.mm@gmail.com>
2023-11-02 12:26:59 -0700

    TS listadoArticulos

--- src/app/listadoarticulos/listadoarticulos.component.ts
+++ src/app/listadoarticulos/listadoarticulos.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-listadoarticulos',
   templateUrl: './listadoarticulos.component.html',
   styleUrls: ['./listadoarticulos.component.css']
 })
-export class ListadoarticulosComponent {
+export class ListadoarticulosComponent implements OnInit{
+  @Input() datos:any;
+  
+  constructor() { }
 
+  ngOnInit() {
+  }
 }
